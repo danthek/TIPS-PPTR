fdff9d141698bf30af59ea90860a4586143e3e65
danthek <abraham8507@gmail.com>
2022-09-01 18:46:13 -0500

    generate screenshot pdf

--- screenShot.js
+++ screenShot.js
@@ -4,6 +4,9 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('http://www.example.com', { waitUntil: 'networkidle0' }); //will wait until there are no more running requests in the browser, yhat means that the website is full loaded
-  await page.screenshot({ path: 'page.png', fullPage: 'true' });
+  await page.screenshot({
+    path: 'examplePageScreenshot.png',
+    fullPage: 'true',
+  });
   await browser.close();
 })();
